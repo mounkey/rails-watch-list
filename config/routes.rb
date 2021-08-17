2fb9d73aad855984a06d3ecf5da6664ea41caf7c
juan grover <jpgrover@gmail.com>
2021-08-17 19:43:40 -0400

    middle exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
+  get 'list/index'
+  get 'list/show'
+  get 'list/new'
+  get 'list/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :list, only: [:new, :create, :show, :index ]
 end
