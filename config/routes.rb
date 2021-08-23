64a8716e283918bf40874e57d22f0f44d7a67705
juan grover <jpgrover@gmail.com>
2021-08-23 16:57:39 -0400

    Listo desafio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, expect: [:destroy, :update] do
+  resources :lists, except: [:destroy, :update] do
     resources :bookmarks, only:[:new, :create]
   end
   resources :bookmarks, only:[:destroy]
